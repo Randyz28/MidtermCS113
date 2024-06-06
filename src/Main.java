689004480de73f5cc11daab2df1a8b4a70414d16
randy <randyzam2899@gmail.com>
2024-06-05 22:02:50 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// ch 1 - 7
+// not much from ch 1
+// ch 2 - using data and expressions to solve a problem (strings, ints, doubles, floats, booleans, etc..)
+// Ch 3 - not much API stuff, no wrapper class or graphics
+// ch 4 - def classes, instance data, like project 1, most of ch 4 but not graphics
+// ch 5 - for loops and while loops, if statements, arrays, boolean relational operators, expect math related problems like sum or product of an array
+// ch 6 - method overloading, not much from static modifier,
+// ch 7 - arrays, pretty much most of ch 7, not much ArrayList class
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
